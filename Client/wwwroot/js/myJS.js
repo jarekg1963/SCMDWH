fd05230f14260f7a525e972a2b1abd8bc6b76397
jarekg1963 <jarekg@test.com.pl>
2023-04-25 11:06:49 +0200

    Wydruki gotowe

---
+++ Client/wwwroot/js/myJS.js
@@ -0,0 +1,12 @@
+﻿function Print() {
+    $(".hideWhenPrint").hide();
+    window.print();
+    $(".hideWhenPrint").show();
+}
+
+function SaveLultureLS() {
+    window.blazorCulture = {
+        get: () => window.localStorage['BlazorCulture'],
+        set: (value) => window.localStorage['BlazorCulture'] = value
+    };
+}
